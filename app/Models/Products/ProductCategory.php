063493f02542c4a38c6fc8ef87aac6beacce0d1f
thuann-vn <thuann@evolableasia.vn>
2018-05-14 14:27:42 +0700

    Update framework

---
+++ app/Models/Products/ProductCategory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\Products;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class ProductCategory extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'product_categories';
+}
