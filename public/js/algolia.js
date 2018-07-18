ecaa388b8e7512ca3d9e4d44da9098f39fa64e25
ngocthua92 <ngocthua92@live.com>
2018-07-18 22:14:33 +0700

    Update landing page

--- public/js/algolia.js
+++ public/js/algolia.js
@@ -1,5 +1,5 @@
 (function() {
-    var client = algoliasearch('P53SZUIML8', '19f54cccab5efbeb0d351b307326a864');
+    var client = algoliasearch('5R1L0G38JG', '547ce5d98108c2b5b19e37e91073fb75');
     var index = client.initIndex('products');
     var enterPressed = false;
     //initialize autocomplete on search input (ID selector must match)
@@ -19,9 +19,6 @@
                                 ${suggestion._highlightResult.name.value}
                             </span>
                             <span>$${(suggestion.price / 100).toFixed(2)}</span>
-                        </div>
-                        <div class="algolia-details">
-                            <span>${suggestion._highlightResult.details.value}</span>
                         </div>
                     `;
 
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -11,7 +11,7 @@
                 </div>
                 <div class="search-form">
                     <form>
-                        <input type="text" name="keyword" class="input-lg" placeholder="{{__('frontend.header.search_placeholder')}}">
+                        <input type="text" id="aa-search-input" name="keyword" class="input-lg" placeholder="{{__('frontend.header.search_placeholder')}}">
                         <button type="submit" class="search-submit"><i class="fa fa-search"></i></button>
                     </form>
                 </div>
