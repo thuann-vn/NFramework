57a581121389762fafa9fc0be281f0766a51eace
thuann-vn <thuann@evolableasia.vn>
2018-09-28 15:52:29 +0700

    Add theme support

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 //        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        VoyagerThemes\VoyagerThemesServiceProvider::class,
     ],
 
     /*
