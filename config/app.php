37ab47c34646ac4904dda090f9367dd2c701ccb9
thuann-vn <thuann@evolableasia.vn>
2018-10-26 16:36:52 +0700

    [dev] Remove unused packages

--- config/app.php
+++ config/app.php
@@ -160,7 +160,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 //        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
-        VoyagerThemes\VoyagerThemesServiceProvider::class,
     ],
 
     /*
